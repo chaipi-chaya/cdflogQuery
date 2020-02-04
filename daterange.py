d1c8f3e22b56677cffad01b226b40261d0688c9f
Chaya <titee15017@gmail.com>
2020-02-04 18:40:15 -0500

    Update daterange.py

--- daterange.py
+++ daterange.py
@@ -3,4 +3,8 @@ import sys, time, os, json, datetime
 for root, dirs, files in os.walk('data'):
     path = root.split(os.sep)
     for fn in files:
-        print(root, fn)
+        fp = root + os.sep + fn
+        f = open(fp, 'r')
+        for line in f:
+            data = json.load(line)
+            
