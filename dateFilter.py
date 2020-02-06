e6699a023de64834d4f9af31ff753baba4777281
Chaya Chaipitakporn <chaipi.chaya@gmail.com>
2020-02-06 18:44:02 -0500

    ;)

---
+++ dateFilter.py
@@ -0,0 +1,31 @@
+import sys,time,os,json,datetime,operator
+
+#SELECT * FROM cddata WHERE Date > 2019-01-01 and Date < 2019-01-02
+txtdata = []
+for root,dirs, files in os.walk('data'):
+    #print(root,dirs,files)
+    path = root.split(os.sep)
+    for fn in files:
+        fp = root+os.sep+fn
+        f = open(fp, 'r')
+        for line in f:
+            data = json.loads(line)
+            payload = data['payload']
+            payload = json.loads(payload)
+            k = payload['received'].split('T')[0]
+            date = k.split('-')
+            
+            if (date[0] == '2019') and (date[1] == '01') and ((date[2] == '01') or (date[2] == '02')):
+                txtdata.append(json.dumps(data))
+                    
+                    
+# sorted_dc = sorted(dateCount.items(),key=operator.itemgetter(1),reverse=True)
+
+file = open("datawithin2019-01-01-2019-01-02.txt", "w") 
+for i in txtdata:
+    file.write(str(i) + '\n' ) 
+file.close() 
+                    
+                    
+            
+            
--- daterange.py
+++ daterange.py
@@ -1,9 +1,27 @@
-import sys, time, os, json, datetime
+import sys,time,os,json,datetime,operator
 
-for root, dirs, files in os.walk('data'):
+#SELECT * FROM cddata WHERE Date > 2019-01-01 and Date < 2019-01-20
+dateCount = {}
+for root,dirs, files in os.walk('data'):
+    #print(root,dirs,files)
     path = root.split(os.sep)
     for fn in files:
-        fp = root + os.sep + fn
+        fp = root+os.sep+fn
+        print(fp)
         f = open(fp, 'r')
         for line in f:
-            data = json.load(line)
+            data = json.loads(line)
+            payload = data['payload']
+            payload = json.loads(payload)
+#            print(payload['received'])
+            k = payload['received'].split('T')[0]
+            #print (k)
+            if k in dateCount.keys():
+                dateCount[k]+=1
+            else:
+                dateCount[k] = 1
+
+# 0 == key , 1 == value
+sorted_dc = sorted(dateCount.items(),key=operator.itemgetter(1),reverse=True)
+
+print(sorted_dc)
