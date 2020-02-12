b9a36b4c271fbc00783b14e580f2410426d9becf
Chaya Chaipitakporn <chaipi.chaya@gmail.com>
2020-02-11 23:51:31 -0500

    almost

--- dataFilterPitime.py
+++ dataFilterPitime.py
@@ -1,33 +1,53 @@
 import sys,time,os,json,datetime,operator
 
-#SELECT * FROM cddata WHERE Date > 2019-01-01 and Date < 2019-01-02
-txtdata = []
-for root,dirs, files in os.walk('data'):
-    #print(root,dirs,files)
+# SELECT temp_5, temp_8 FROM print_reading WHERE pitime > 2018-05-06 16:00:00 AND pitime < 2018-05-06 18:00:00;
+# SELECT AVG(temp_8) FROM print_reading WHERE pitime > 2018-05-05 16:00:00 AND pitime < 2018-05-07 16:00:00;
+
+temp_5data = []
+temp_8data = []
+temp_8avgdata = []
+
+datetemp1 = datetime.datetime(2018, 5, 6, 16, 00)
+datetemp2 = datetime.datetime(2018, 5, 6, 18, 00)
+dateavg1 = datetime.datetime(2018, 5, 5, 16, 00)
+dateavg2 = datetime.datetime(2018, 5, 7, 16, 00)
+
+
+for root,dirs, files in os.walk('datalog'):
+    # print(root,dirs,files)
     path = root.split(os.sep)
     for fn in files:
         fp = root+os.sep+fn
         f = open(fp, 'r')
         for line in f:
             data = json.loads(line)
-            payload = data['payload']
-            payload = json.loads(payload)
-            k = payload['received'].split('T')[0]
-            date = k.split('-')
-            
-            if (date[0] == '2019') and (date[1] == '01') and ((date[2] == '01') or (date[2] == '02')):
-                txtdata.append(json.dumps(data))
+            pitime = data['pitime']
+            pitimedate = pitime.split('T')[0]
+            pitimetime = pitime.split('T')[1].split('.')[0]
+            date_str = pitimedate + ' ' + pitimetime
+            date = datetime.datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+            if (date > datetemp1) and (date < datetemp2):
+                if 'temp_5' in data:
+                    temp_5data.append(data['temp_5'])
+                if 'temp_8' in data:
+                    temp_8data.append(data['temp_8'])
+            if (date > dateavg1) and (date < dateavg2):
+                if 'temp_8' in data:
+                    temp_8avgdata.append(data['temp_8'])
                     
+print(temp_5data)
+print(temp_8data)
+print(temp_8avgdata)
                     
 # sorted_dc = sorted(dateCount.items(),key=operator.itemgetter(1),reverse=True)
 
-file = open("datawithin2019-01-01-2019-01-02.txt", "w") 
-file.write('') 
-file.close() 
+# file = open("datawithin2019-01-01-2019-01-02.txt", "w") 
+# file.write('') 
+# file.close() 
 
-file = open("datawithin2019-01-01-2019-01-02.txt", "w") 
-for i in txtdata:
-    file.write(str(i) + '\n' ) 
-file.close() 
+#file = open("datawithin2019-01-01-2019-01-02.txt", "w") 
+#for i in txtdata:
+#    file.write(str(i) + '\n' ) 
+#file.close() 
                     
                     
